235edceb1c68f8c1bd8e11047c41b2ef7c707775
unknown <ar_ashish@hotmail.com>
2021-11-12 00:20:26 -0600

    Rating Completed

--- src/main/java/com/localbusinessplatform/LocalBusinessPlatformApplication.java
+++ src/main/java/com/localbusinessplatform/LocalBusinessPlatformApplication.java
@@ -15,13 +15,13 @@ public class LocalBusinessPlatformApplication {
  
 
 public static void main(String[] args) {
-		LbpUtil lbpUtil = new LbpUtil();
-		try {
-			System.out.println(lbpUtil.calculateDistance("6823 N Terra Vista Dr, Peoria, IL", "242-23 91st Ave, Bellerose, NY"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		LbpUtil lbpUtil = new LbpUtil();
+//		try {
+//			System.out.println(lbpUtil.calculateDistance("6823 N Terra Vista Dr, Peoria, IL", "242-23 91st Ave, Bellerose, NY"));
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		SpringApplication.run(LocalBusinessPlatformApplication.class, args);
 	}
 	
